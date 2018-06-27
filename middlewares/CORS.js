5da6873ae7a60ff441b3c8786f77fec7f52f2c2b
tubotao <tubotao@tungee.com>
2018-06-27 13:05:04 +0800

    fix

--- middlewares/CORS.js
+++ middlewares/CORS.js
@@ -1,7 +1,7 @@
 module.exports = function (req, res, next) {
 
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
-  res.setHeader('Access-Control-Allow-Origin', 'http://www.meidengpaper.com'); // Sorry I am so lazy
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Sorry I am so lazy
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('X-Powered-By', 'NodeJS');
